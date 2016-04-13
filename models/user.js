3699acb97524bfb8a98587071837adf6b14296f7
glendex <glendasp@gmail.com>
2016-04-13 13:23:50 -0500

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,24 @@
+/**
+ * Created by glendex on 4/13/16.
+ */
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+    local: {
+        usernamer: String,
+        password: String
+    }
+});
+
+userSchema.methods.generateHash = function(password){
+    //Create salted has of password by hashing plaintext password
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+userSchema.methods.validPassword = function(password){
+    //Hash entered passwrod, compare with store hash
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
